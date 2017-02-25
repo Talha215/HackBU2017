3693208c8164b8565d3c1af887d3f23fc3e77ca0
Charlie Schmitter <schmic6@rpi.edu>
2017-02-25 15:44:13 -0500

    added stuff
    stuff

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  var CONSUMER_KEY = 'HVHJPON1gtWyc8Urbk15jWn55'
+  var CONSUMER_SECRET = 'kdWH5s6mBt7anbHWx5oQkrUrvrQp2ZvtKiu0jSpwWaOxnD1CuR'
+  var ACCESS_TOKEN = '3368125355-xeLBvjdmUcY4BNhQ0EbRocgJkZoUTLGD0YFAuCZ'
+  var ACCESS_SECRET = 'hPJzfpodLEQUlcnn4Q5sOpJbpXSMz0ogR2otU0yVLlz3l'
+}
