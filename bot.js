5b634212380f56eee7fa13f490c0529f17c2138d
Charlie Schmitter <schmic6@rpi.edu>
2017-02-25 18:10:47 -0500

    first bot part done
    title

--- bot.js
+++ bot.js
@@ -2,3 +2,24 @@ var twit = require('twit');
 var config = require('./config.js');
 
 var Twitter = new twit(config);
+var handle = '@facerecogbot';
+
+Twitter.get('search/tweets', { q: handle }, function(err, data, response){
+  for(i = 0; i < data.statuses.length; i++){
+    if(data.statuses[i].extended_entities !== undefined){
+      var url = data.statuses[i].extended_entities.media[0].media_url;
+      console.log(url);
+
+      var fs = require('fs');
+      var request = require('request');
+      var download = function(uri, file, cb){
+        request.head(uri, function(err, res, body){
+          request(uri).pipe(fs.createWriteStream(file)).on('close', cb);
+        });
+      };
+      download(url, 'cat-img.jpg', function(){
+        console.log("downloaded!");
+      });
+    }
+  }
+})
--- config.js
+++ config.js
@@ -1,6 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
 module.exports = {
-  var CONSUMER_KEY = 'HVHJPON1gtWyc8Urbk15jWn55'
-  var CONSUMER_SECRET = 'kdWH5s6mBt7anbHWx5oQkrUrvrQp2ZvtKiu0jSpwWaOxnD1CuR'
-  var ACCESS_TOKEN = '3368125355-xeLBvjdmUcY4BNhQ0EbRocgJkZoUTLGD0YFAuCZ'
-  var ACCESS_SECRET = 'hPJzfpodLEQUlcnn4Q5sOpJbpXSMz0ogR2otU0yVLlz3l'
+  consumer_key: 'Cehf5q0ymF3JhNRF4eHyDi3wn',
+  consumer_secret: '6T9MonTNgF9mCFEcWoXPcJTJTdhrvUTbULyHLH5zU2KMCRzMMa',
+  access_token: '835594930126999553-pmQZ1pKPSRbkqzOHBnjUVYHGxC1DSkO',
+  access_token_secret: 'WYPzuY9RWjOgYi9nnCnkayyaZGCmXXyGdgiBpYv4QpgMm'
 }
