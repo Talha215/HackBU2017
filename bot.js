8ae1317d51d5ab7aa512d5cf7bbb9d8c4fe4aae5
Charlie Schmitter <schmic6@rpi.edu>
2017-02-26 08:58:22 -0500

    base bot kinda done
    :)

--- bot.js
+++ bot.js
@@ -1,48 +1,73 @@
 var twit = require('twit');
 var config = require('./config.js');
+var fs = require('fs');
+var http = require('http');
 
 var Twitter = new twit(config);
 var handle = '@facerecogbot';
 
+function Tweet(path, username){
+  this.path = path;
+  this.username = username;
+}
+
 function createFile(url){
-  var dest = './images/user_submitted';
+  var dest = './images/user_submitted/';
   var suffix = url.substring(url.lastIndexOf('.'));
   var crypto = require('crypto');
   var rand_id = crypto.randomBytes(15).toString('hex');
   var rand_id = rand_id.concat(suffix);
   return dest + rand_id;
-}
+};
 
-function perform(){
-  Twitter.get('search/tweets', { q: handle }, function(err, data, response){
-    for(i = 0; i < data.statuses.length; i++){
-      if(data.statuses[i].extended_entities !== undefined){
-        var url = data.statuses[i].extended_entities.media[0].media_url;
-        var fileName = createFile(url);
+function gatherTweets(){
+  var query = handle
+  Twitter.get('search/tweets', {q: query, result_type: 'recent' }, function(err, data, response){
+    console.log(data);
+    if(data !== undefined){
+      for(i = 0; i < data.statuses.length; i++){
+        if(data.statuses[i].extended_entities !== undefined){
+          var url = data.statuses[i].extended_entities.media[0].media_url;
+          var fileName = createFile(url);
 
-        var http = require('http');
-        var fs = require('fs');
-
-        var download = function(uri, dest, cb){
-          var file = fs.createWriteStream(dest);
-          var request = http.get(uri, function(response){
-            response.pipe(file);
-            file.on('finish', function(){
-              file.close(cb);
+          var download = function(uri, dest, name, cb){
+            var file = fs.createWriteStream(dest);
+            var request = http.get(uri, function(response){
+              response.pipe(file);
+              file.on('finish', function(){
+                file.close(cb);
+              });
             });
-          });
+          }
+
+          var username = data.statuses[i].user.screen_name
+          download(url, fileName, username, function(err, data, response){
+            console.log("downloading to " + fileName);
+            postTweet(fileName, username);
+          })
         }
-
-        download(url, dest + rand_id, function(){
-          console.log("finished downloading to: " + dest + rand_id);
-        });
-
-        Twitter.post('statuses/update', { status: 'tweet content' }, function(err, data, response){
-          console.log(data);
-        })
       }
     }
   });
+};
+
+function postTweet(fileName, username){
+  var b64content = fs.readFileSync(fileName, { encoding: 'base64' });
+
+  Twitter.post('media/upload', { media_data: b64content }, function(err, data, response){
+    var media_id_string = data.media_id_string;
+    var altText = "face recognition bot";
+    var meta_params = { media_id: media_id_string, alt_text: { text: altText } }
+
+    Twitter.post('media/metadata/create', meta_params, function(err, data, response){
+      if(!err){
+        var params = { status: '@' + username + " here is your photo! #facerecogbot", media_ids: [media_id_string] }
+        Twitter.post('statuses/update', params, function(err, data, response){
+          console.log(data);
+        });
+      }
+    });
+  });
 }
 
-setInterval(perform(), 60000);
+gatherTweets();
